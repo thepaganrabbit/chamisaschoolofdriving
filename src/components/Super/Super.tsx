07a275a300d4e6729c97b8b9a8d8cf0974e09c96
thepaganrabbit <gonzalesgustavoc@gmail.com>
2024-01-17 11:55:55 -0700

    feat: banner added with image. #2

---
+++ src/components/Super/Super.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Super.scss';
+import car from '../../assets/carfree2.jpg';
+
+const Super = (): React.ReactElement => {
+  return (
+    <section>
+      <div className="container-md super">
+        <img src={car} className="img-fluid" style={{height: '20rem', width: '100%', objectFit: 'cover', objectPosition: 'top right'}} alt="..." />
+        <div className="brand">
+          <h1>Chamisa School of Driving</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Super;
