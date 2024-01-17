7a472c36563660f6c1291b2633c306a9922eb93e
thepaganrabbit <gonzalesgustavoc@gmail.com>
2024-01-17 09:50:18 -0700

    feat: added navbar and setup basic route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
     </div>
   );
 }
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TopNav from '../../components/TopNav/TopNav';
+
+const Home = (): React.ReactElement => {
+  return (<TopNav />);
+};
+
+export default Home;
